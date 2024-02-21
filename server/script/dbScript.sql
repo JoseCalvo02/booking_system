033db1f9cf0378e186149339e7d114bf63b544c5
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-02-20 23:00:05 -0600

    Creacion de mas tablas SQL

--- server/script/dbScript.sql
+++ server/script/dbScript.sql
@@ -55,3 +55,83 @@ create table Estilistas (
 );
 
 ALTER TABLE Estilistas ADD CONSTRAINT fk_rolID_3 FOREIGN KEY(RolID) REFERENCES Roles(rolID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/****** -------------------------------------	Table Catalogo	---------------------------------- ******/
+create table catalogo (
+	servicioID INT PRIMARY KEY IDENTITY(1,1),
+	nombreServicio varchar(50),
+	descripcion varchar(150),
+	precio INT
+);
+
+/****** -------------------------------------	Table galeriaEstilos	---------------------------------- ******/
+
+create table galeriaEstilos (
+	estiloID INT PRIMARY KEY IDENTITY(1,1),
+	nombreEstilo varchar(50),
+	rutaImagen varchar(150)
+);
+
+/****** -------------------------------------	Table detalleServicio	---------------------------------- ******/
+
+create table detalleServicio (
+	detalleID INT PRIMARY KEY IDENTITY(1,1),
+	servicioID INT,
+	tiempoEstimado TIME
+);
+
+ALTER TABLE detalleServicio ADD CONSTRAINT fk_servicioID FOREIGN KEY(servicioID) REFERENCES catalogo(servicioID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/****** -------------------------------------	Table disponibilidadTiempoReal	---------------------------------- ******/
+
+create table disponibilidadTiempoReal (
+	disponibilidadID INT PRIMARY KEY IDENTITY(1,1),
+	estilistaID	INT,
+	diaSemana VARCHAR(20),
+	horaInicio TIME,
+	horaFinal TIME
+);
+
+ALTER TABLE disponibilidadTiempoReal ADD CONSTRAINT fk_estilistaID FOREIGN KEY(estilistaID) REFERENCES estilistas(estilistaID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ -------------------------------------	Table citas	---------------------------------- ******/
+
+ create table citas (
+	citaID INT PRIMARY KEY IDENTITY(1,1),
+	clienteID	INT,
+	estilistaID INT,
+	servicioID INT,
+	fechaCita DATETIME,
+	estiloID INT,
+	comentarios varchar(150)
+);
+
+ALTER TABLE citas ADD CONSTRAINT fk_clienteID FOREIGN KEY(clienteID) REFERENCES clientes(clienteID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE citas ADD CONSTRAINT fk_estilistaID_2 FOREIGN KEY(estilistaID) REFERENCES estilistas(estilistaID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE citas ADD CONSTRAINT fk_servicioID_2 FOREIGN KEY(servicioID) REFERENCES catalogo(servicioID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE citas ADD CONSTRAINT fk_estiloID FOREIGN KEY(estiloID) REFERENCES galeriaEstilos(estiloID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ -------------------------------------	Table citasReagendadas	---------------------------------- ******/
+
+create table citasReagendadas (
+	reagendadaID INT PRIMARY KEY IDENTITY(1,1),
+	citaID	INT,
+	idCitaNueva INT,
+	fechaNuevaCita DATETIME,
+	motivo varchar(150)
+);
+
+ALTER TABLE citasReagendadas ADD CONSTRAINT fk_citaID FOREIGN KEY(citaID) REFERENCES citas(citaID) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+ -------------------------------------	Table bloqueoHorarios	---------------------------------- ******/
+ create table bloqueoHorarios (
+	bloqueoID INT PRIMARY KEY IDENTITY(1,1),
+	estilistaID	INT,
+	dia VARCHAR(20),
+	horaInicio TIME,
+	horaFinal TIME,
+	motivo varchar(150)
+);
+
+ALTER TABLE bloqueoHorarios ADD CONSTRAINT fk_estilistaID_3 FOREIGN KEY(estilistaID) REFERENCES estilistas(estilistaID) ON DELETE NO ACTION ON UPDATE NO ACTION;
