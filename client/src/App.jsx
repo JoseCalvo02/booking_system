d2178e1ca544f9f2aaceca1a7228e51b2389b378
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-02-25 17:59:34 -0600

    Add routes for Admin, Stylist, and Client roles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,21 @@ import 'react-toastify/dist/ReactToastify.css';
 import Home from "./pages/Home/Home"
 import NotFound from "./pages/Layout/NotFound";
 import Auth from "./pages/Auth/Auth"
+import Admin from "./pages/Roles/Admin/Admin"
+import Stylist from "./pages/Roles/Stylist/Stylist"
+import Client from "./pages/Roles/Client/Client"
 
 function App() {
     return (
         <>
             <Routes>
-                {/*
-                    Define las rutas y los componentes asociados. Por ejemplo, cuando la URL es "/", se renderiza el componente Home.
-                */}
+                {/* Define las rutas y los componentes asociados. Por ejemplo, cuando la URL es "/", se renderiza el componente Home. */}
                 <Route path="/" element={<Home />} />
                 <Route path="/auth" element={<Auth />} />
-                {/*
-                    La ruta comodín "*" maneja todas las URL que no coinciden con las rutas anteriores.
-                */}
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/stylist" element={<Stylist />} />
+                <Route path="/client" element={<Client />} />
+                {/* La ruta comodín "*" maneja todas las URL que no coinciden con las rutas anteriores. */}
                 <Route path="*" element={<NotFound/>} />
             </Routes>
             {/* Agrega el ToastContainer aquí para que esté disponible en toda la aplicación. */}
