ce01175240a1a1c0c070e92e85eb84e4fd9d177b
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-02-26 23:18:23 -0600

    Update styles in MainContent and Sidebar components

--- client/src/components/Shared/Sidebar/Sidebar.jsx
+++ client/src/components/Shared/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
     return (
         <aside className='bg-red-500'>
-            <h1 >Sidebar</h1>
+            <h1>Sidebar</h1>
         </aside>
     )
 }
