c74f16b18e2cc8c6343e72f8deab22bf24702ac9
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-02-27 21:45:11 -0600

    Update background colors and add custom box shadow

--- client/src/pages/Roles/Admin/Admin.jsx
+++ client/src/pages/Roles/Admin/Admin.jsx
@@ -4,7 +4,7 @@ import MainContent from '../../../components/Roles/Admin/MainContent'
 
 export default function Admin() {
     return (
-        <div className='grid min-h-screen grid-cols-6'>
+        <div className='grid h-screen grid-cols-6 bg-[#f6f6f9]'>
             <Sidebar />
             <MainContent />
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,11 +9,12 @@ export default {
             primary_h: '#064ACB',
             gre: '#00df9a',
             gre_h: '#008066',
-            w: '#fff',
-            g: '#b6b6b6',
             eje: { //Ejemplo de una clase dentro de los colores
                 p: '#b6b6b6',
-            }
+            },
+        },
+        boxShadow: {
+          custom: '0 3px 10px rgb(0,0,0,0.2)',
         },
     },
 },
