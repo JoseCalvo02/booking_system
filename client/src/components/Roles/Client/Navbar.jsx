cc54165da1aabb8d64706741b18e5f101f69030d
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-02-27 22:20:32 -0600

    actualizacion de paquetes

--- client/src/components/Roles/Client/Navbar.jsx
+++ client/src/components/Roles/Client/Navbar.jsx
@@ -53,19 +53,19 @@ export default function Navbar() {
                 {/* Opciones de Navegación */}
                 <ul className={`fixed flex flex-col items-center justify-center w-[80%] md:w-full xl:w-full h-full gap-5 top-0 xl:justify-end xl:items-end xl:flex-row xl:static md:justify-end md:items-end md:flex-row md:static transition-all duration-500 ${showMenu ? "left-0 bg-gray-900" : "-left-full"}`}>
                     {/* Opciones de HOME */}
-                    <li className='flex justify-center p-4 transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
+                    <li className='flex justify-center p-4 text-white transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
                         <NavLink to="/" className={`text-w p-2 hover:bg-blue-950 hover:rounded-lg ${location.pathname === "/" ? "md:bg-blue-900 md:rounded-lg" : ""}`}>
                             Home
                         </NavLink>
                     </li>
                     {/* Opciones de SERVICES */}
-                    <li className='flex justify-center p-4 transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
+                    <li className='flex justify-center p-4 text-white transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
                         <NavLink to="/services" className={`text-w p-2 hover:bg-blue-950 hover:rounded-lg ${location.pathname === "/services" ? "md:bg-blue-900 md:rounded-lg" : ""}`}>
                             Services
                         </NavLink>
                     </li>
                     {/* Opciones de PROFILE */}
-                    <li className='relative flex items-center justify-center p-4 transition-colors duration-500 md:p-0 md:transition-none md:border-none'>
+                    <li className='relative flex items-center justify-center p-4 text-white transition-colors duration-500 md:p-0 md:transition-none md:border-none'>
                         <button
                         className={`text-w p-2 hover:bg-blue-950 hover:rounded-lg ${location.pathname === "/Profile" ? "md:bg-blue-900 md:rounded-lg" : ""}`}
                         onMouseEnter={() => setShowDropdown(true)}
