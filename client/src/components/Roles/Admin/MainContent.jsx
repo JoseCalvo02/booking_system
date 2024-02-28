4270f08f08a8da69582f62779b9bf013018e8187
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-02-27 21:45:31 -0600

    Refactor component styles and update sidebar icons

--- client/src/components/Roles/Admin/MainContent.jsx
+++ client/src/components/Roles/Admin/MainContent.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function MainContent() {
     return (
-        <main className='col-span-5 bg-blue-500'>
-            <header className='flex flex-col w-full gap-4 p-4 bg-green-400 md:flex-row h-[6%]'>
+        <main className='flex flex-col w-full col-span-5'>
+            <header className='px-8 py-4 mx-6 mt-6 mb-2 bg-white shadow-custom rounded-xl'>
                 <h1>Header</h1>
             </header>
 
-            <section className='flex h-[94%]'>
-                <div className='w-2/3 p-4 bg-green-500'>
+            <section className='flex flex-grow gap-8 m-6'>
+                <div className='w-3/4 p-8 bg-white shadow-custom rounded-xl'>
                     <h2>Content</h2>
                 </div>
 
-                <div className='w-1/3 p-4 bg-green-600'>
+                <div className='w-1/4 p-8 bg-white shadow-custom rounded-xl'>
                     <h3>Sub Content</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, ducimus velit dolorem autem cupiditate dolores quasi! Fugit, dicta laboriosam magni illo quia laborum dolor. Suscipit ex, perspiciatis mollitia numquam velit autem laborum accusamus laboriosam ea dolorem quo nobis nesciunt repellendus dignissimos cupiditate quia tenetur enim tempora corporis adipisci. Rem, ipsa.</p>
                 </div>
--- client/src/components/Shared/Sidebar/Sidebar.jsx
+++ client/src/components/Shared/Sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import customStyles from '../../../custom/customStyles';
-import { MdOutlineSpaceDashboard, MdOutlineLogout } from "react-icons/md";
+import {
+    TbLayoutGrid, TbCalendarEvent, TbUserSearch, TbNotebook, TbUserEdit , TbCalendarUser, TbCoins, TbReport, TbSettings, TbLogout2
+} from "react-icons/tb";
+
 
 function Sidebar() {
     return (
-        <aside className='p-8 text-lg font-semibold text-white bg-gray-900'>
+        <aside className='flex flex-col p-8 text-lg font-semibold text-white bg-gray-900 rounded-r-2xl'>
             {/* Logo */}
             <div className='flex gap-1 p-2 mb-4 text-2xl'>
                 <h1 className='text-white'>Studio</h1>
@@ -13,27 +16,66 @@ function Sidebar() {
             </div>
 
             {/* Navigation */}
-            <div>
-                <nav>
-                    <ul className=''>
+            <div className='flex flex-grow '>
+                <nav className='flex flex-grow '>
+                    <ul className='flex flex-col flex-grow text-lg'>
                         <li>
                             <a className={customStyles.a} href="">
-                                <MdOutlineSpaceDashboard />
+                                <TbLayoutGrid size={20}/>
                                 Dashboard
                             </a>
                         </li>
                         <li>
-                            <a href="">Users</a>
-                        </li>
-                        <li>
-                            <a href="">Roles</a>
-                        </li>
-                        <li>
-                            <a href="">Permissions</a>
-                        </li>
-                        <li className=''>
                             <a className={customStyles.a} href="">
-                                <MdOutlineLogout />
+                                <TbCalendarEvent size={20}/>
+                                Citas
+                            </a>
+                        </li>
+                        <li>
+                        <a className={customStyles.a} href="">
+                                <TbUserSearch size={20}/>
+                                Clientes
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbNotebook size={20}/>
+                                Catálogo
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbUserEdit  size={20}/>
+                                Estilistas
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbCalendarUser size={20}/>
+                                Horarios
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbCoins  size={20}/>
+                                Canjes
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbReport  size={20}/>
+                                Reportes
+                            </a>
+                        </li>
+                        <li>
+                            <a className={customStyles.a} href="">
+                                <TbSettings  size={20}/>
+                                Perfil
+                            </a>
+                        </li>
+                        <li className='mt-auto'>
+                            <a className={customStyles.a} href="">
+                                <TbLogout2 size={20}/>
                                 Log out
                             </a>
                         </li>
