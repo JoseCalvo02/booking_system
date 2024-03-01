02eca27142e0c28b4715b50fc39d2311ac2ce2bf
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-03-01 00:01:22 -0600

    Cambio en la pantalla Cleint

--- client/src/components/Roles/Client/Navbar.jsx
+++ client/src/components/Roles/Client/Navbar.jsx
@@ -3,7 +3,10 @@ import { useState, useEffect } from "react";
 
 //Icons
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import { CgProfile } from "react-icons/cg";
+import { CgProfile, CgLogOut, CgToolbox } from "react-icons/cg";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { TbReport, TbSettings, TbLayoutGrid } from "react-icons/tb";
+
 
 export default function Navbar() {
     const location = useLocation();
@@ -55,15 +58,15 @@ export default function Navbar() {
                     {/* Opciones de HOME */}
                     <li className='flex justify-center p-4 text-white transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
                         <NavLink to="/" className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg'>
-                            <CgProfile className="mr-1" />
+                            <TbLayoutGrid className="mr-1" size={20} />
                             Home
                         </NavLink>
                     </li>
                     {/* Opciones de SERVICES */}
                     <li className='flex justify-center p-4 text-white transition-colors duration-500 border-b border-gray-600 md:p-0 md:transition-none md:border-none'>
                         <NavLink to="/services" className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg' >
-                            <CgProfile className="mr-1" />
-                            Services
+                            <CgToolbox className="mr-1" size={20} />
+                            Servicios
                         </NavLink>
                     </li>
                     {/* Opciones de PROFILE */}
@@ -73,35 +76,35 @@ export default function Navbar() {
                         onMouseEnter={() => setShowDropdown(true)}
                         onMouseLeave={() => setShowDropdown(false)}
                         >
-                            <div className='relative flex items-center justify-center p-4 text-white transition-colors duration-500 md:p-0 md:transition-none md:border-none'>
-                                <CgProfile className="mr-1" />
-                                <p>Profile</p>
+                            <div className='relative flex items-center justify-center p-4 mr-16 text-white transition-colors duration-500 md:p-0 md:transition-none md:border-none'>
+                                <CgProfile className="mr-1" size={20} />
+                                <p>Perfil</p>
                             </div>
                             { /* Dropdown Options */}
                             {showDropdown && (
                                 <ul className='absolute left-0 w-40 mt-2 bg-gray-900 rounded-lg' onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
                                     <li className='flex p-2'>
                                         <NavLink to='/settings' className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg'>
-                                            <CgProfile className='mr-1' />
-                                            Settings
+                                            <TbSettings className='mr-1' size={20} />
+                                            Configuración
                                         </NavLink>
                                     </li>
                                     <li className='flex p-2'>
                                         <NavLink to="/reports" className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg'>
-                                            <CgProfile className="mr-1" />
-                                            Reports
+                                            <TbReport className="mr-1" size={20} />
+                                            Reportes
                                         </NavLink>
                                     </li>
                                     <li className='flex p-2'>
                                         <NavLink to="/rewards" className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg'>
-                                            <CgProfile className="mr-1" />
-                                            Rewards
+                                            <FaMoneyBillAlt className="mr-1" size={20} />
+                                            Recompensas
                                         </NavLink>
                                     </li>
                                     <li className='flex p-2'>
                                         <NavLink to="/logout" className='flex items-center p-2 text-w hover:bg-blue-950 hover:rounded-lg'>
-                                            <CgProfile className="mr-1" />
-                                            Logout
+                                            <CgLogOut  className="mr-1" size={20} />
+                                            Log Out
                                         </NavLink>
                                     </li>
                                 </ul>
--- client/src/pages/Roles/Client/Client.jsx
+++ client/src/pages/Roles/Client/Client.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../../components/Roles/Client/Navbar'
 import MainContent from '../../../components/Roles/Client/mainContent'
+import ContentCards from '../../../components/Roles/Client/ContentCards'
 
 function Client() {
   return (
     <div>
       <Navbar />
       <MainContent />
+      <ContentCards/>
     </div>
   )
 }
