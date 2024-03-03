2905c1c3ed534473f437d3a53a6cce4e566aa151
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-03-03 08:43:32 -0600

    Cambio en el Hero video y texto

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import { ReactTyped } from 'react-typed';
+import VideoHome from '../../assets/VideoHome.mp4';
 
 function Hero() {
     return (
-        <section className="text-black">
-            <div className="max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-                <p className="p-2 font-bold uppercase text-md md:text-xl text-primary">
-                    Lorem ipsum dolor sit amet consectetur.
-                </p>
-                <h1 className="text-4xl font-bold md:text-7xl sm:text-6xl md:py-6">
-                    Los mejores servicios.
-                </h1>
+        <section className="h-screen overflow-hidden">
+            <video autoPlay loop muted className="absolute inset-0 z-0 object-cover w-full h-full filter brightness-70" src={VideoHome}></video>
+            <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-white">
+                <div className="max-w-[800px] w-full mx-auto text-center">
+                    <p className="p-2 font-bold text-gray-400 uppercase text-md md:text-xl">
+                        Dale color a tu vida
+                    </p>
+                    <h1 className="text-4xl font-bold md:text-7xl sm:text-6xl md:py-6">
+                        Con Fabi Studio
+                    </h1>
                 <div className='flex items-center justify-center '>
                     <p className="py-4 text-xl font-bold md:text-4xl sm:text-3xl">
-                        Lorem ipsum dolor sit
+                        Contamos con
                     </p>
                     <ReactTyped
-                        className='pl-2 text-xl font-bold md:pl-4 md:text-4xl sm:text-3xl text-primary'
+                        className='pl-2 text-xl font-bold text-gray-400 md:pl-4 md:text-4xl sm:text-3xl '
                         strings={['Esmaltado.', 'Pedicure.', 'Mucho Mas.']} typeSpeed={120} backSpeed={140} loop
                     />
                 </div>
 
-                <p className='text-xl font-bold text-gray-500 md:text-2xl'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, fugit!
-                </p>
-                <button className='bg-primary w-[150px] rounded-md font-medium my-6 mx-auto py-3 text-white hover:bg-primary_h'>Comenzemos</button>
+                    <p className='text-xl font-bold text-gray-400 md:text-2xl'>
+                        Encuentra tu estilo en cada detalle. Bienvenido(a) a Fabi Studio
+                    </p>
+                    <button className='bg-primary w-[150px] rounded-md font-medium my-6 py-3 text-white hover:bg-primary_h'>Comenzemos</button>
+                </div>
             </div>
         </section>
     );
