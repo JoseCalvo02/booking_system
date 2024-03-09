2cf37a0302476a8a5c012afd50a78d36b02c965e
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-03-09 17:19:42 -0600

    Update token expiration time to 1 hour and moved SQL script to docs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -107,7 +107,7 @@ export const loginUser = async (req, res) => {
         });
 
         // Define la duración del token en segundos
-        const expiresIn = 900; // 15min
+        const expiresIn = 3600; // 1h
 
         // Genera el token JWT con la información del usuario y la clave secreta del archivo .env
         const token = jwt.sign({ userId: user.id, nombre: user.nombre, email: user.correo, role: roleName.nombreRol }, process.env.JWT_SECRET, { expiresIn });
