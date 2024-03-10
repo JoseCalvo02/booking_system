4a1051328a2a7c9717d065de6c81671ac16355d0
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-03-09 21:48:50 -0600

    Add client list and fetch functionality

--- client/src/components/Roles/Admin/Clients.jsx
+++ client/src/components/Roles/Admin/Clients.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getClients } from '../../../../api/adminApi';
+import customStyles from '../../../custom/customStyles';
 
 const Clients = () => {
+    const [clients, setClients] = useState([]); // Define el estado local para almacenar los usuarios
+
+    useEffect(() => {
+        // Llamar a la función para obtener todos los usuarios
+        const fetchClients = async () => {
+            try {
+                const clients = await getClients();
+                setClients(clients); // Establecer el estado local con los usuarios obtenidos
+            } catch (error) {
+                console.error('Error al obtener los usuarios:', error.message);
+            }
+        };
+
+        fetchClients(); // Llamar a la función para obtener todos los usuarios
+    }, []);
+
+
     return (
         <>
-            <div className='w-full p-8 bg-white shadow-custom rounded-xl'>
+            <div className='w-full h-full p-8 bg-white shadow-custom rounded-xl'>
                 <h2>Clients</h2>
+
+                {/* Table */}
+                <div className='overflow-y-auto max-h-[75vh]'>
+                    <table className='w-full '>
+                        <thead className='sticky top-0 z-10 text-white bg-gray-900'>
+                            {/* Table Header */}
+                            <tr className=''>
+                                <th className={customStyles.th}>ID</th>
+                                <th className={customStyles.th}>Nombre</th>
+                                <th className={customStyles.th}>Apellido</th>
+                                <th className={customStyles.th}>Telefono</th>
+                                <th className={customStyles.th}>Email</th>
+                                <th className={customStyles.th}>Direccion</th>
+                                <th className={customStyles.th}>Points</th>
+                                <th className={customStyles.th}>Estado</th>
+                                <th className={customStyles.th}>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody className='overflow-y-auto'>
+                            {/* Map over clients and render each client as a table row */}
+                            {clients.map((client) => (
+                                <tr key={client.id} className='hover:bg-gray-100'>
+                                    <td className={customStyles.td}>{client.id}</td>
+                                    <td className={customStyles.td}>{client.name}</td>
+                                    <td className={customStyles.td}>{client.lastName}</td>
+                                    <td className={customStyles.td}>{client.phone}</td>
+                                    <td className={customStyles.td}>{client.email}</td>
+                                    <td className={customStyles.td}>{client.address}</td>
+                                    <td className={customStyles.td}>{client.points}</td>
+                                    <td className={customStyles.td}>{client.status}</td>
+                                    <td className={customStyles.td}>
+                                        <button className='p-2 text-white bg-green-500 rounded-lg'>Edit</button>
+                                        <button className='p-2 text-white bg-red-500 rounded-lg'>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </>
     );
--- client/src/custom/customStyles.jsx
+++ client/src/custom/customStyles.jsx
@@ -7,7 +7,11 @@ const customStyles = {
     button: 'rounded-[20px] border border-solid border-primary_h bg-primary text-white font-bold text-xs py-3 px-11 tracking-[1px] uppercase transition-transform duration-75 ease-in focus:outline-none active:scale-95',
     error: 'm-0 text-xs text-red-600',
     // Styles used in Admin.jsx
-    a: 'flex items-center gap-4 p-2 rounded-lg hover:bg-blue-950',
+    link: 'flex items-center gap-4 p-2 rounded-lg hover:bg-blue-950',
+    span: 'hidden lg:block',
+    // Styles used in Admin/Clients.jsx
+    th: 'p-2',
+    td: 'p-2 border border-gray-200 text-center overflow-x-auto max-w-[100px] overflow-y-auto max-h-[10px] whitespace-nowrap',
 };
 
 export default customStyles;
