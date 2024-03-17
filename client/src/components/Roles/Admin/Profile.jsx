5c39b0b1dffbdfde17841639a278c0495c9cbeaa
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-03-17 11:19:37 -0600

    Pull info from the token in the Profile page

--- client/src/components/Roles/Admin/Profile.jsx
+++ client/src/components/Roles/Admin/Profile.jsx
@@ -1,66 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 const Profile = () => {
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        address: '',
+        phone: '',
+    }); // Define el estado local para almacenar el nombre de usuario
+
+    useEffect(() => {
+        // Decodificar el token JWT para obtener la información del usuario, como el nombre
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            console.log(decodedToken);
+            setUserData({
+                name: decodedToken.nombre,
+                email: decodedToken.email,
+                phone: decodedToken.telefono,
+                address: decodedToken.direccion
+            }); // Establecer el nombre de usuario en el estado local
+        }else{
+            setUserData({
+                name: 'Usuario',
+                email: '',
+                phone: '',
+                address: ''
+            });
+        }
+    }, []);
+
     return (
-        <>
-             <div className='w-full h-full py-4 pl-4 pr-8 bg-white shadow-custom rounded-xl'>
-                <div class="grid grid-cols-6 pt-3 sm:grid-cols-10">
+        <div className='h-full py-8 pl-4 pr-8 bg-white shadow-custom rounded-xl'>
+            <div className="grid grid-cols-6 sm:grid-cols-10">
+                {/* Side options */}
+                <aside className="hidden col-span-2 sm:block">
+                    <ul>
+                        <li className="px-2 py-2 mt-5 font-semibold transition border-l-2 cursor-pointer border-l-primary text-primary hover:text-primary">Cuenta</li>
+                    </ul>
+                </aside>
 
-                    <div class="col-span-2 hidden sm:block">
-                        <ul>
-                            <li class="mt-5 cursor-pointer border-l-2 border-l-primary px-2 py-2 font-semibold text-primary transition hover:border-l-primary hover:text-primary">Cuenta</li>
-                            <li class="mt-5 cursor-pointer border-l-2 px-2 py-2 font-semibold text-primary transition hover:border-l-primary hover:text-primary">Cuenta</li>
-                        </ul>
+                {/* Main content */}
+                <main className="col-span-8 overflow-hidden sm:border-l sm:px-8">
+                    {/* Fisrt section */}
+                    <div className="py-4 mb-6 border-b border-gray-200">
+                        <h1 className="py-2 text-2xl font-semibold">Configuracion de cuenta</h1>
+                        <p className="font- text-slate-600">Ingrese los datos que desea configurar</p>
                     </div>
-
-                    <div class="col-span-8 overflow-hidden rounded-xl sm:px-8 sm:shadow-md">
-                        <div class="pt-4">
-                            <h1 class="py-2 text-2xl font-semibold">Configuracion de cuenta</h1>
-                            <p class="font- text-slate-600">Ingrese los datos que desea configurar</p>
-                        </div>
-                    <hr class="mt-4 mb-8" />
-                    <p class="py-2 text-xl font-semibold">Correo electronico</p>
-                    <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                        <p class="text-gray-600">Su correo electronico es: <strong>mcorderoulate@gmial.com</strong></p>
-                        <button class="inline-flex text-sm font-semibold text-primary hover:underline decoration-2">Cambiar Correo</button>
-                    </div>
-                    <hr class="mt-4 mb-8" />
-                    <p class="py-2 text-xl font-semibold">Contraseña</p>
-                    <div class="flex items-center">
-                        <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-                            <label for="login-password">
-                                <span class="text-sm text-gray-500">Contraseña actual</span>
-                                <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-primary">
-                                    <input type="password" id="login-password" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="********" />
-                                </div>
-                            </label>
-                            <label for="login-password">
-                                <span class="text-sm text-gray-500">Nueva contraseña</span>
-                                <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-primary">
-                                    <input type="password" id="login-password" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="********" />
-                                </div>
-                            </label>
+                    {/* Second section */}
+                    <div className='py-4 mb-6 border-b border-gray-200'>
+                        <p className="py-2 text-xl font-semibold">Correo electronico</p>
+                        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+                            <p className="text-gray-600">Su correo electronico es: <strong>{}</strong></p>
+                            <button className="inline-flex text-sm font-semibold text-primary hover:underline decoration-2">Cambiar Correo</button>
                         </div>
                     </div>
-                    <p class="mt-2">
-                        No recuerdas tu contraseña  actual.
-                        <a class="text-sm font-semibold text-primary hover:underline decoration-2" href="#">Recuperar contraseña</a>
-                    </p>
-                    <button class="mt-4 rounded-lg bg-primary px-4 py-2 text-white">Guardar contraseña</button>
-                    <hr class="mt-4 mb-8" />
+                    {/* Third section */}
+                    <div className='py-4 mb-6 border-b border-gray-200'>
+                        <p className="py-2 text-xl font-semibold">Contraseña</p>
+                        <div className="flex items-center">
+                            <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+                                <label htmlFor="login-password">
+                                    <span className="text-sm text-gray-500">Contraseña actual</span>
+                                    <div className="relative flex overflow-hidden transition border-2 rounded-md focus-within:border-primary">
+                                        <input type="password" id="login-password" className="flex-shrink w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border-gray-300 appearance-none focus:outline-none" placeholder="********" />
+                                    </div>
+                                </label>
+                                <label htmlFor="login-password">
+                                    <span className="text-sm text-gray-500">Nueva contraseña</span>
+                                    <div className="relative flex overflow-hidden transition border-2 rounded-md focus-within:border-primary">
+                                        <input type="password" id="login-password" className="flex-shrink w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border-gray-300 appearance-none focus:outline-none" placeholder="********" />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <span className="mt-2"> No recuerdas tu contraseña  actual.</span>
+                            <button className="text-sm font-semibold text-primary hover:underline decoration-2" href="#">Recuperar contraseña</button>
+                        </div>
+                        <button className="px-4 py-2 mt-4 text-white rounded-lg bg-primary">Guardar contraseña</button>
+                    </div>
 
-                    <div class="mb-10">
-                        <p class="py-2 text-xl font-semibold">Eliminar cuenta personal</p>
-                        <p class="inline-flex items-center rounded-lg bg-rose-100 px-4 py-1 text-rose-600">
-                            Proceda con precaucion
+                    {/* Fourth section */}
+                    <div className="mb-10">
+                        <p className="py-2 text-xl font-semibold">Eliminar cuenta personal</p>
+                        <p className="inline-flex items-center px-4 py-1 rounded-lg bg-rose-100 text-rose-600">
+                            Proceda con precaución
                         </p>
-                        <p class="mt-2">Asegúrese de haber realizado una copia de seguridad de su cuenta en caso de que alguna vez necesite acceder a sus datos. Borraremos completamente sus datos. No hay forma de acceder a su cuenta después de esta acción.</p>
-                        <button class="ml-auto text-sm font-semibold text-rose-600 hover:underline decoration-2">Continue con la eliminacion</button>
+                        <p className="mt-2">Asegúrese de haber realizado una copia de seguridad de su cuenta en caso de que alguna vez necesite acceder a sus datos. Borraremos completamente sus datos. No hay forma de acceder a su cuenta después de esta acción.</p>
+                        <button className="ml-auto text-sm font-semibold text-rose-600 hover:underline decoration-2">Continue con la eliminación</button>
                     </div>
-                    </div>
-                </div>
-            </div>
-        </>
+                </main>
+            </div> {/* End of grid */}
+        </div>
     );
 }
 
