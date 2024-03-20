bdd156759269201e5da7d0dac1ca4bfa0823520d
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2024-03-19 21:43:11 -0600

    Refactor Clients component table layout

--- client/src/components/Roles/Admin/Clients.jsx
+++ client/src/components/Roles/Admin/Clients.jsx
@@ -21,53 +21,51 @@ const Clients = () => {
 
 
     return (
-        <>
-            <div className='w-full h-full p-8 bg-white shadow-custom rounded-xl'>
-                <h1 className='mb-4 text-2xl font-semibold text-center text-gray-900 border border-black rounded-md shadow-custom border-opacity-5'>
-                    Tabla de clientes
-                </h1>
+        <div className='w-full h-full p-8 bg-white shadow-custom rounded-xl'>
+            <h1 className='mb-4 text-2xl font-semibold text-center text-gray-900 border border-black rounded-md shadow-custom border-opacity-5'>
+                Tabla de clientes
+            </h1>
 
-                {/* Table */}
-                <div className='overflow-y-auto max-h-[75vh]'>
-                    <table className='w-full '>
-                        <thead className='sticky top-0 z-10 text-white bg-gray-900'>
-                            {/* Table Header */}
-                            <tr className=''>
-                                <th className={customStyles.th}>ID</th>
-                                <th className={customStyles.th}>Nombre</th>
-                                <th className={customStyles.th}>Apellido</th>
-                                <th className={customStyles.th}>Telefono</th>
-                                <th className={customStyles.th}>Email</th>
-                                <th className={customStyles.th}>Direccion</th>
-                                <th className={customStyles.th}>Points</th>
-                                <th className={customStyles.th}>Estado</th>
-                                <th className={customStyles.th}>Actions</th>
+            {/* Table */}
+            <div className='overflow-y-auto max-h-[75vh]'>
+                <table className='w-full '>
+                    <thead className='sticky top-0 z-10 text-white bg-gray-900'>
+                        {/* Table Header */}
+                        <tr className=''>
+                            <th className={customStyles.th}>ID</th>
+                            <th className={customStyles.th}>Nombre</th>
+                            <th className={customStyles.th}>Apellido</th>
+                            <th className={customStyles.th}>Telefono</th>
+                            <th className={customStyles.th}>Email</th>
+                            <th className={customStyles.th}>Direccion</th>
+                            <th className={customStyles.th}>Points</th>
+                            <th className={customStyles.th}>Estado</th>
+                            <th className={customStyles.th}>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody className='overflow-y-auto'>
+                        {/* Map over clients and render each client as a table row */}
+                        {clients.map((client) => (
+                            <tr key={client.usuarioID} className='hover:bg-gray-100'>
+                                <td className={customStyles.td}>{client.usuarioID}</td>
+                                <td className={customStyles.td}>{client.nombre}</td>
+                                <td className={customStyles.td}>{client.apellidos}</td>
+                                <td className={customStyles.td}>{client.telefono}</td>
+                                <td className={customStyles.td}>{client.correo}</td>
+                                <td className={customStyles.td}>{client.direccion}</td>
+                                <td className={customStyles.td}>0</td>
+                                <td className={customStyles.td}>{client.estado}</td>
+                                <td className={customStyles.td}>
+                                    <button className='p-2 mr-2 text-white bg-green-400 rounded-lg hover:bg-green-500'>Edit</button>
+                                    <button className='p-2 text-white bg-red-400 rounded-lg hover:bg-red-500'>Delete</button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody className='overflow-y-auto'>
-                            {/* Map over clients and render each client as a table row */}
-                            {clients.map((client) => (
-                                <tr key={client.usuarioID} className='hover:bg-gray-100'>
-                                    <td className={customStyles.td}>{client.usuarioID}</td>
-                                    <td className={customStyles.td}>{client.nombre}</td>
-                                    <td className={customStyles.td}>{client.apellidos}</td>
-                                    <td className={customStyles.td}>{client.telefono}</td>
-                                    <td className={customStyles.td}>{client.correo}</td>
-                                    <td className={customStyles.td}>{client.direccion}</td>
-                                    <td className={customStyles.td}>0</td>
-                                    <td className={customStyles.td}>{client.estado}</td>
-                                    <td className={customStyles.td}>
-                                        <button className='p-2 mr-2 text-white bg-green-400 rounded-lg hover:bg-green-500'>Edit</button>
-                                        <button className='p-2 text-white bg-red-400 rounded-lg hover:bg-red-500'>Delete</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
+                        ))}
+                    </tbody>
+                </table>
             </div>
-        </>
+
+        </div>
     );
 }
 
