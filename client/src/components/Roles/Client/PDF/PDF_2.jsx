15726c3d6408382e6881006b1ba5e07f9878780b
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-03-21 23:04:58 -0600

    Add PDF download functionality for client reports

---
+++ client/src/components/Roles/Client/PDF/PDF_2.jsx
@@ -0,0 +1,25 @@
+import {
+    Document,
+    Text,
+    Page,
+} from '@react-pdf/renderer';
+
+function PDF_2() {
+    return (
+        <Document>
+            <Page>
+                <Text>Report PDF 2</Text>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni reiciendis dolorem ipsum corporis nobis distinctio odio aliquam
+                     quia dicta mollitia eius quidem eveniet molestias fugiat dolorum sed commodi, minus saepe pariatur, consequuntur porro sapiente ut nemo? 
+                     Esse distinctio repellendus aliquid suscipit vitae. Sunt culpa asperiores consequuntur aliquid assumenda dolor! Maxime eum voluptate nostrum 
+                     nulla vel nemo saepe ex est, sunt consequatur esse quam, provident officia molestiae. Totam odio sed recusandae eos est delectus alias doloremque 
+                     cumque, et quo eligendi maxime deleniti magni consectetur. Nisi unde hic saepe cumque harum tempore assumenda perspiciatis iusto? Optio at dolore nam, 
+                     eligendi ipsa et velit doloremque possimus totam cumque facere quas dolor ipsum culpa obcaecati temporibus voluptas quaerat quae sint exercitationem 
+                     minima. Commodi ex nihil repellat excepturi laborum pariatur ducimus, rem, perspiciatis consectetur minima quia tenetur quis exercitationem sit quos 
+                     praesentium enim consequuntur reiciendis impedit aspernatur tempore. Voluptatibus id voluptatem adipisci delectus vel.</Text>
+            </Page>
+        </Document>
+    );
+}
+
+export default PDF_2;
