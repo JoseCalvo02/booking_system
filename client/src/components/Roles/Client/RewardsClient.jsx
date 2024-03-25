ee65fccc5760cc7e4102147473c74cf8794e3f8d
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-03-24 18:22:07 -0600

    Refactor user profile and rewards display

--- client/src/components/Roles/Client/RewardsClient.jsx
+++ client/src/components/Roles/Client/RewardsClient.jsx
@@ -30,12 +30,12 @@ await RedeemedModal();
 }
 
     return (
-        <section className="flex flex-grow w-full h-screen py-8 shadow-custom rounded-xl">
-            <div className="w-1/2 h-screen mt-28">
-                <div className="p-4 rounded-lg md:p-8">
-                    <div className="max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-black sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
-                        <h1 className="mb-4 text-3xl font-bold text-center">Perfil de Usuario</h1>
-                        <hr className="mb-4"></hr>
+        <section className="flex flex-col w-full min-h-screen py-8 md:flex-row shadow-custom rounded-xl">
+            <div className="w-full h-full mt-8 md:w-1/2 md:mt-0 ">
+                <div className="h-full p-4 rounded-lg md:p-8 ">
+                    <div className="h-full max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-black sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
+                        <h1 className="mb-4 text-3xl font-bold text-center md:text-left">Perfil de Usuario</h1>
+                        <hr className="w-full mb-4"></hr>
                         <table className="w-full mb-4 text-left table-fixed">
                             <tbody>
                                 <tr>
@@ -52,16 +52,16 @@ await RedeemedModal();
                                 </tr>
                             </tbody>
                         </table>
-                        <hr className="mt-4"></hr>
+                        <hr className="w-full mt-auto"></hr>
                     </div>
                 </div>
             </div>
 
-            <div className="w-1/2 h-screen mt-28">
-            <div className="p-4 rounded-lg md:p-8">
-                    <div className="max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-black sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
-                        <h1 className="mb-4 text-3xl font-bold text-center">Recompensas Disponibles</h1>
-                        <hr className="mb-4"></hr>
+            <div className="w-full h-full mt-8 md:w-1/2 md:mt-0">
+                <div className="h-full p-4 rounded-lg md:p-8">
+                    <div className="h-full max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-black sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
+                        <h1 className="mb-4 text-3xl font-bold text-center md:text-left">Recompensas Disponibles</h1>
+                        <hr className="w-full mb-4"></hr>
                         <table className="w-full mb-4 text-left table-fixed">
                             <tbody>
                                 <tr>
@@ -69,10 +69,10 @@ await RedeemedModal();
                                     <td className="w-2/3 px-4 py-2 font-bold">5,000 Puntos</td>
                                     <td className="w-2/3 px-4 py-2 font-bold">
                                         <a onClick={handleRedeem} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        Canjear
-                                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
+                                            Canjear
+                                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                            </svg>
                                         </a>
                                     </td>
                                 </tr>
@@ -81,22 +81,22 @@ await RedeemedModal();
                                     <td className="w-2/3 px-4 py-2 font-bold">10,000 puntos</td>
                                     <td className="w-2/3 px-4 py-2 font-bold">
                                         <a onClick={handleRedeem} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        Canjear
-                                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
+                                            Canjear
+                                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                            </svg>
                                         </a>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td className="w-1/3 px-4 py-2 font-medium">100% Descuento</td>
-                                    <td className="w-2/3 px-4 py-2 font-bold">15,000 Puntos</td>
+                                    <td className="w-1/3 px-4 py-2 font-medium">75% Descuento</td>
+                                    <td className="w-2/3 px-4 py-2 font-bold">15,000 puntos</td>
                                     <td className="w-2/3 px-4 py-2 font-bold">
                                         <a onClick={handleRedeem} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        Canjear
-                                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
+                                            Canjear
+                                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                            </svg>
                                         </a>
                                     </td>
                                 </tr>
