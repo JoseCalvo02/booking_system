3e4770e10a9954c2193ec0a3719f83cdbef3facf
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-03-30 01:22:16 -0600

    Update styling for CitasPage table

--- client/src/components/Client/CitasPage.jsx
+++ client/src/components/Client/CitasPage.jsx
@@ -5,7 +5,7 @@ function CitasPage() {
         <section className="flex flex-col w-full h-screen py-8 mx-auto md:w-3/4 md:flex-row md:py-0 md:space-x-8">
             <div className="w-full p-10 mx-auto md:w-full">
                 <div className="overflow-x-auto">
-                    <table className="w-full mx-auto mt-8 overflow-hidden text-sm text-left text-black bg-gray-100 rounded-lg shadow-md md:mt-36">
+                    <table className="w-full mx-auto mt-8 overflow-hidden text-sm text-left text-black bg-gray-100 rounded-lg shadow-md md:mt-24">
                         <caption className="mb-4 text-xl font-bold text-center">Citas del Usuario</caption>
                         <thead className="text-xs uppercase bg-gray-200">
                             <tr>
@@ -24,44 +24,44 @@ function CitasPage() {
                                 <th scope="col" className="px-6 py-3 md:px-8">
                                     Estilista
                                 </th>
-                                <th scope="col" className="px-6 py-3 md:px-8">
+                                <th scope="col" className="px-6 py-3 text-center md:px-8">
                                     Accion
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className="text-black border">
-                                <td className="px-6 py-4 md:px-8 md:py-6">
+                                <td className="px-6 md:px-8 ">
                                     <div className="flex items-center">
-                                        <div className="text-base font-semibold">01</div>
+                                        <p>01</p>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 md:px-8 md:py-6">
+                                <td className="px-6 md:px-8">
                                     <div className="flex items-center">
-                                        <div className="text-base font-semibold">22/10/2021</div>
+                                        <p>22/10/2021</p>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 md:px-8 md:py-6">
+                                <td className="px-6 md:px-8">
                                     <div className="flex items-center">
-                                        <div className="text-base font-semibold">10:00</div>
+                                        <p>10:00</p>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 md:px-8 md:py-6">
+                                <td className="px-6 md:px-8 ">
                                     <div className="flex items-center">
-                                        <div className="text-base font-semibold">Corte de Cabello</div>
+                                        <p>Corte de Cabello</p>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 md:px-8 md:py-6">
+                                <td className="px-6 md:px-8 ">
                                     <div className="flex items-center">
-                                        <div className="text-base font-semibold">Juan Perez</div>
+                                        <p>Juan Perez</p>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 md:px-8 md:py-6">
-                                    <div className="flex items-center space-x-3">
-                                        <button className="px-6 py-3 font-medium text-white rounded-lg bg-primary hover:bg-primary_h md:w-[200px]">
+                                <td className="p-2 px-6 md:px-8">
+                                    <div className="flex items-center justify-center space-x-3">
+                                        <button className="px-6 py-2 font-medium text-white rounded-lg bg-primary hover:bg-primary_h md:w-[200px]">
                                             Reprogramar
                                         </button>
-                                        <button className="px-6 py-3 font-medium text-white rounded-lg bg-primary hover:bg-primary_h md:w-[200px]">
+                                        <button className="px-6 py-2 font-medium text-white rounded-lg bg-primary hover:bg-primary_h md:w-[200px]">
                                             Cancelar
                                         </button>
                                     </div>
