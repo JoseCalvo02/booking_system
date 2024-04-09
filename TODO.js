c1889e58e65b7e51d208319fff5ca194ecb499ae
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-04-08 23:38:52 -0600

    TODO COMMENT

--- TODO.js
+++ TODO.js
@@ -1,6 +1,6 @@
 /*
   ! FIX BUGS:
-  * 1. ERROR import RedeemedModal from '../Modals/Coupons/RedeemedModal', siempre da error en la vista, sin embargo la funcion como tal si funciona en el proceso de CANJE.;
+  * 1. En recompensas, al canjear un cupon no actualiza el token nuevo actualizado para poder ver los puntos actualizados en la pagina, hay que salir y entrar nuevamente para ver el cambio.
 
   TODO:
   * 1. Add a new task to the list.
