5eafa8d17e01124a947adcb0eaa42330666650ed
Mario Cordero Ulate <91304423+Macordero2021@users.noreply.github.com>
2024-04-08 23:22:27 -0600

    Fix bug where the user can't add a new task in TODO.js

--- TODO.js
+++ TODO.js
@@ -1,6 +1,6 @@
 /*
   ! FIX BUGS:
-  * 1. Fix the bug where the user can't add a new task.
+  * 1. ERROR import RedeemedModal from '../Modals/Coupons/RedeemedModal', siempre da error en la vista, sin embargo la funcion como tal si funciona en el proceso de CANJE.;
 
   TODO:
   * 1. Add a new task to the list.
